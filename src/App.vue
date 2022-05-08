2b87f12295702b46ad46a2bde46bc4d81cfe36cd
Caio <bladellano@gmail.com>
2022-05-07 21:32:09 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{title}}</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quisquam.</p>
+  <div v-if="showModal">
+    <!-- <Modal
+    :header="header"
+    :text="text"
+    theme="sale"
+    @close="toggleModal"
+    /> -->
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+          <a href="#">sign up now</a>          
+          <a href="#">more info</a>          
+      </template>
+      <h1>Ninja Givaway!</h1>
+      <p>Grab yout ninja sawg for falf price</p>
+    </Modal>
+  </div>
+  <button @click.alt="toggleModal">Open Modal (alt)</button>
+  <input type="text" ref="name">
+  <button @click="handleClick" >Click me</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Modal from './components/Modal'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components: {Modal},
+  data(){
+    return {
+      title: 'Meu primeiro app vie data vue',
+      header:'Sign up for the Giveaway!',
+      text:'Grab you ninja sawf for half price!',
+      showModal:false
+    }
+  },
+  methods:{
+    handleClick(){
+      alert('Hello');
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+
+    },
+    toggleModal(){
+      this.showModal = !this.showModal
+    }
   }
-}
+};
 </script>
 
 <style>
@@ -22,5 +60,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1{
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+  color: brown;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="theme">
+      <!-- <h1>{{header}}</h1>
+      <p>{{text}}</p> -->
+     <slot>default content</slot>
+     <div class="actions">
+       <slot name="links"></slot>
+     </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    // props:['header','text','theme'],
+    props:['theme'],
+    methods:{
+        closeModal(){
+            this.$emit('close')
+        }
+    }
+};
+</script>
+
+<style>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background-color: white;
+  border-radius: 10px;
+}
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+.modal h1{
+    color:blueviolet;
+    border: none;
+    padding: 0;
+}
+.modal p {
+    font-style: normal;
+}
+.modal.sale{
+    background-color: crimson;
+    color: white;
+}
+.modal.sale h1{
+    color:white
+}
+
+.modal.dark{
+    background-color: black;
+    color: white;
+}
+.modal.dark h1{
+    color:white
+}
+.modal .actions{
+  text-align: center;
+  margin: 30px 0 10px 0;
+}
+.modal .actions a{
+  color: #333;
+  padding: 8px;
+  border: 1px solid #eee;
+  border-radius: 4px;
+  text-decoration: none;
+  margin: 10px;
+}
+
+.modal.dark .actions,
+.modal.sale .actions{
+  color: white;
+}
+.modal.dark .actions a,
+.modal.sale .actions a{
+  color: white;
+}
+</style>
